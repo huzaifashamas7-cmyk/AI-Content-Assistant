9e8f2d2388a80fa248f59b179e1ffd257ddc4a5a
Muhammad-Huzaifa-Shamas-dev <huzaifashamas7@gmail.com>
2026-09-04 21:27:17 +0500

    Replace model options with openai/gpt-oss-120b

--- app.py
+++ app.py
@@ -27,8 +27,7 @@ with st.sidebar:
     model = st.selectbox(
         "Groq Model",
         [
-            "llama-3.1-8b-instant",
-            "llama-3.3-70b-versatile",
+            "openai/gpt-oss-120b"
         ],
         index=0,
     )
